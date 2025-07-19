22c047a9d4768d190462df8f5a8f1fc7753ad2b4
rajeshpranav7 <rajeshpranav705@gmail.com>
2025-07-19 17:05:11 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   name    = "hcl-vpc"
   cidr    = "10.0.0.0/24"
-  azs     = ["us-west-1]"
+  azs     = ["us-west-1"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 }
@@ -17,7 +17,6 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "devops-eks"
   cluster_version = "1.33"
-  subnets         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 }
 
