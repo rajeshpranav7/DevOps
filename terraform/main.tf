f4c3d8f51a9acc4456af9aa1642f82d430398169
rajeshpranav7 <rajeshpranav705@gmail.com>
2025-07-19 16:40:25 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,9 @@ provider "aws" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  name    = "devops-vpc"
-  cidr    = "10.0.0.0/16"
-  azs     = ["us-east-1a", "us-east-1b"]
+  name    = "hcl-vpc"
+  cidr    = "10.0.0.0/24"
+  azs     = ["us-west-1]"
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 }
@@ -22,5 +22,5 @@ module "eks" {
 }
 
 resource "aws_ecr_repository" "app" {
-  name = "appointment-app"
+  name = "devops-eks/appointment-app"
 }
